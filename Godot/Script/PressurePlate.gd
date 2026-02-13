7225afc649952f266abdc4f1f144acf5ae533b0b
jessepletchercb <jessepletchercb@gmail.com>
2026-02-13 16:19:37 -0700

    GOdot

---
+++ Godot/Script/PressurePlate.gd
@@ -0,0 +1,12 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		print("Player triggered")
